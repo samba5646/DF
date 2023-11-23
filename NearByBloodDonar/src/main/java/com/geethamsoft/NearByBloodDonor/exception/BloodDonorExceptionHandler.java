dc2d061db19b82aab926854c46924a6da0fb4978
parthiban <parthiban.geetham@gmail.com>
2023-11-23 11:41:20 +0530

    exception handler added in blood donor

--- NearByBloodDonar/src/main/java/com/geethamsoft/NearByBloodDonor/exception/BloodDonorExceptionHandler.java
+++ NearByBloodDonar/src/main/java/com/geethamsoft/NearByBloodDonor/exception/BloodDonorExceptionHandler.java
@@ -1,4 +1,21 @@
 package com.geethamsoft.NearByBloodDonor.exception;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
 public class BloodDonorExceptionHandler {
+
+    @ExceptionHandler(value = MethodArgumentNotValidException.class)
+    public ResponseEntity<List<?>> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        record Errors(String field, String message){};
+        List<Errors> errors = ex.getBindingResult().getFieldErrors().stream().map((fe) ->{
+            return new Errors(fe.getField(),fe.getDefaultMessage());
+        }).toList();
+        return ResponseEntity.ok(errors);
+    }
 }
